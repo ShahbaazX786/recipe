e37470efa38c9ad53576c0335391e1d56d39f5e4
ShahbaazX786 <shahbaazplatinum@gmail.com>
2026-01-05 16:49:51 +0530

    Calling the cronjob via server.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,18 @@
 import express from "express";
+import renderTickerJob from "./config/cron.js";
 import { ENV } from "./config/env.js";
-import recipeRoutes from './routes/recipe.route.js'
+import recipeRoutes from "./routes/recipe.route.js";
 
 const app = express();
 const PORT = ENV.PORT;
+if (ENV.NODE_ENV === "production") renderTickerJob.start();
 
-app.use(express.json())
+app.use(express.json());
+app.use("/api/v1/favorites", recipeRoutes);
 
 app.get("/api/v1/health", (req, res) => {
   res.status(200).json({ success: true, message: "Server is up and running" });
 });
-
-app.use('/api/v1/favorites', recipeRoutes);
 
 app.listen(PORT, () => {
   console.log("Server Running at PORT:", PORT);
