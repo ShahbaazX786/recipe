24e298ee4524d4a95a64e1667bfef42131e0be76
ShahbaazX786 <shahbaazplatinum@gmail.com>
2026-01-05 15:24:15 +0530

    Added a health check api in server.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,10 @@ import { ENV } from "./config/env.js";
 const app = express();
 const PORT = ENV.PORT;
 
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({ success: true, message: "Server is up and running" });
+});
+
 app.listen(PORT, () => {
   console.log("Server Running at PORT:", PORT);
 });
