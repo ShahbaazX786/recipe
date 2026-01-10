b0021b39e06082d45e0a9be2b939d39ae66359ca
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-10 14:36:18 +0530

    Bug fix: Updated background of the safe area view so that the top bar of the app doesn't look different from the rest of the app.

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { COLORS } from "@/constants/colors";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
@@ -6,7 +7,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
         <Slot />
       </SafeAreaView>
     </ClerkProvider>
