2d564fdb623f7dd2c583c81cd71af78222ff82c4
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-10 17:45:09 +0530

    Added dummy recipe detail page.

---
+++ frontend/app/recipe/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Teent = () => {
+  return (
+    <View>
+      <Text>Teent</Text>
+    </View>
+  );
+};
+
+export default Teent;
