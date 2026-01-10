42865c17c7b23a78cb9d23ddefb630fe2b24169f
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-10 14:11:51 +0530

    Added auth layout

---
+++ frontend/app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect, Stack } from "expo-router";
+
+export default function AuthRoutesLayout() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) return null;
+
+  if (isSignedIn) {
+    return <Redirect href={"/(tabs)/home"} />;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
